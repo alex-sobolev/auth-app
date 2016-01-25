0f05590527d584ba08891425bb29d0dc1cc7231c
alex-sobolev <alex.sobolev1@gmail.com>
2016-01-25 21:24:23 +0300

    Added mongoDB database

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
 
 app.locals.pretty = true;
 app.set('view engine', 'jade');
+
+// connect to database
+mongoose.connect('mongodb://localhost/auth');
+var schema = mongoose.Schema;
+var objectId = schema.objectId;
+var user = mongoose.model('user', new schema({
+  id: objectId,
+  login: String,
+  email: {type: String, unique: true},
+  password: String
+}));
 
 // middleware
 app.use(bodyParser.urlencoded({extended: true}));
